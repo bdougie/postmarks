36944a9c791bd15e40ef0abd9809838899b2c97a
Brianllamar <ilikerobot@gmail.com>
2014-02-08 10:22:04 -0500

    properly embed thumbnail links

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,4 +2,8 @@ class Bookmark < ActiveRecord::Base
    attr_accessible :title, :body, :tag_list, :url, :thumbnail_url
    acts_as_taggable
    
+   def thumbnail_url
+   		Embedly::API.new.oembed( url: self.url).first.thumbnail_url
+   end
+
 end
