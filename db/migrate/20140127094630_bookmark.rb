c911e142a354fbd902b0d0cea8f2699dcaa4692a
Brianllamar <ilikerobot@gmail.com>
2014-02-08 20:39:07 -0500

    removed deleted files

--- db/migrate/20140127094630_bookmark.rb
+++
@@ -1,9 +0,0 @@
-class Bookmark < ActiveRecord::Migration
-  create_table :Bookmarks do |t|
-
-  t.string :title
-  t.text :body
-
-  t.timestamps
-	end
-end
--- db/migrate/20140129115312_create_bookmarks_tags_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateBookmarksTagsTable < ActiveRecord::Migration
-  
-  create_table :bookmarks_tags do |t|
-      t.belongs_to :bookmark
-      t.belongs_to :tag
-    end
-
-end
