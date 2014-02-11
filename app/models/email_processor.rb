a45b8378989a66004fdd5f8f57cc876cb39c45b7
Brianllamar <ilikerobot@gmail.com>
2014-02-10 21:42:19 -0500

    update griddler

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -2,5 +2,11 @@ class EmailProcessor
 	def self.processor(email)
 		# all of your application-specific code here - creating models,
 		# processing reports, etc
+
+		Rails.logger.log params[:envelope][:to] # print the to field to the logs
+    Rails.logger.log params[:subject] # print the subject to the logs
+    Rails.logger.log params[:plain] # print the decoded body plain to the logs if present
+    Rails.logger.log params[:html] # print the html decoded body to the logs if present
+
 	end
 end
