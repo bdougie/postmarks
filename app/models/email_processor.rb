05bdbedd71f5fc603f50fcf57e33936309b11444
Brianllamar <ilikerobot@gmail.com>
2014-02-11 22:32:02 -0500

    added cloudmalin and griddler functionality

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -2,11 +2,10 @@ class EmailProcessor
 	def self.processor(email)
 		# all of your application-specific code here - creating models,
 		# processing reports, etc
-
-		Rails.logger.log params[:envelope][:to] # print the to field to the logs
-    Rails.logger.log params[:subject] # print the subject to the logs
-    Rails.logger.log params[:plain] # print the decoded body plain to the logs if present
-    Rails.logger.log params[:html] # print the html decoded body to the logs if present
-
+		user = User.find_by_email(email.from)
+    bookmark = user.bookmarks.new
+    bookmark.url = email.subject
+    bookmark.description = email.body
+    bookmark.save
 	end
 end
